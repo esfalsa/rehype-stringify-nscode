e134db29078ecf3f6d5e4e34d255a14b7fefdb26
esfalsa <pronoun.nationstates@gmail.com>
2023-06-04 17:23:38 -0700

    chore: add basic test

---
+++ test/index.spec.ts
@@ -0,0 +1,81 @@
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import { test, expect } from "vitest";
+import remarkStringifyNSCode from "../dist/index";
+
+test("commonmark", () => {
+  const file = unified().use(remarkParse).use(remarkStringifyNSCode)
+    .processSync(`
+*Italic*
+
+**Bold**
+
+# Heading 1
+
+## Heading 2
+
+[Link](http://a.com)
+
+![Image](http://url/a.png)
+
+> Blockquote
+
+* List
+* List
+* List
+
+1. One
+2. Two
+3. Three
+
+Horizontal rule:
+
+---
+
+\`Inline code\` with backticks	
+
+\`\`\`
+# code block
+print '3 backticks or'
+print 'indent 4 spaces'
+\`\`\`
+`);
+
+  expect(file.value).toEqual(`[i]Italic[/i]
+
+[b]Bold[/b]
+
+Heading 1
+
+Heading 2
+
+[url=http://a.com]Link[/url]
+
+[img]http://url/a.png[/img]
+
+[quote]Blockquote[/quote]
+
+[list]
+[*]List
+[*]List
+[*]List
+[/list]
+
+[list=1]
+[*]One
+[*]Two
+[*]Three
+[/list]
+
+Horizontal rule:
+
+[hr]
+
+Inline code with backticks
+
+[pre]
+# code block
+print '3 backticks or'
+print 'indent 4 spaces'
+[/pre]`);
+});
